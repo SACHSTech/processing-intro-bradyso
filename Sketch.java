a5c8aeaa76d94295f3a4a7b83244c13970230c21
Brady So <>
2022-02-16 18:54:26 +0000

    add mouse drawn circles

--- Sketch.java
+++ Sketch.java
@@ -25,12 +25,18 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    stroke(128); // color
+    line(150, 25, 270, 350); // x and y locations
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    stroke(255); // color
+    line(50, 125, 70, 50);  // x and y locations
+
+    if (mousePressed) {
+    fill(0);
+    } else {
+    fill(255);
+    } ellipse(mouseX, mouseY, 80, 80); // draw a ellispe at a certain location, x and y location of the mouse
+    }
   
   // define other methods down here.
 }
